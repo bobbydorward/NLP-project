eb66a89d003d4738def9ab025769d70973427419
wubleu <rwilsonic@gmail.com>
2016-12-06 23:26:40 -0500

    nearly finished transitionngrams

--- TransitionNGrams.py
+++ TransitionNGrams.py
@@ -3,28 +3,36 @@ import counts
 from collections import defaultdict
 
 
-class transition_n_gram:
+class transition_n_grams:
 
 	folder = "reuters/training"
 	document = "reuters/test/15549"
+	ngramfrequencies = defaultdict(int)
 
-	def __init__(self, documentcounts):
-		self.docs = documentcounts.docs
-		self.ngramfrequencies = {}
-		for doc in self.docs :
+	def __init__(self, documentcounts, document):
+		docs = documentcounts.docs + document.body
+		for doc in docs :
 			prevline = None
 			for line in doc :
+				line = line[:-1]
 				if prevline != None :
-					ngramfrequencies[(prevline[len(prevline)-1], line[0])] += 1
+					self.ngramfrequencies[(prevline[len(prevline)-1], line[0])] += 1
 				prevline = line
 
 	def order(self, summary) :
-		transitionweights = {}
+		transitionweights = defaultdict(int)
 		for prevsentenceindex in range(len(summary)) :
 			for nextsentenceindex in range(len(summary)) :
 				if (prevsentenceindex != nextsentenceindex) :
 					endword = summary[prevsentenceindex][-1]
 					startword = summary[nextsentenceindex][0]
-					transitionweights[(prevsentenceindex, nextsentenceindex)] = self.ngramfrequencies[(endword, startword)]
+					freq = self.ngramfrequencies[(endword.lower(), startword.lower())]
+					if freq == None :
+						freq = 0
+					transitionweights[(prevsentenceindex, nextsentenceindex)] = freq
+		for prevsentenceindex in range(len(summary)) :
+			for nextsentenceindex in range(prevsentenceindex+1, len(summary)) :
+				if transitionweights[(prevsentenceindex, nextsentenceindex)] > transitionweights[(prevsentenceindex, prevsentenceindex+1)] :
+					summary.insert(prevsentenceindex+1, summary[nextsentenceindex])
 		print(transitionweights)
 
