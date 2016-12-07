c90b64ef3b6a7e2f07a554174a4eb8610055261a
wubleu <rwilsonic@gmail.com>
2016-12-06 21:45:28 -0500

    Began ordering function

--- TransitionNGrams.py
+++ TransitionNGrams.py
@@ -18,6 +18,13 @@ class transition_n_gram:
 					ngramfrequencies[(prevline[len(prevline)-1], line[0])] += 1
 				prevline = line
 
-	def order()
-
+	def order(self, summary) :
+		transitionweights = {}
+		for prevsentenceindex in range(len(summary)) :
+			for nextsentenceindex in range(len(summary)) :
+				if (prevsentenceindex != nextsentenceindex) :
+					endword = summary[prevsentenceindex][-1]
+					startword = summary[nextsentenceindex][0]
+					transitionweights[(prevsentenceindex, nextsentenceindex)] = self.ngramfrequencies[(endword, startword)]
+		print(transitionweights)
 
